a0372d7be836e21d5541cd442fead4a048b823b9
Gabriel Silva <gabrielvsilva29022008@gmail.com>
2024-09-14 23:24:06 -0300

    APP.PY updated

--- app.py
+++ app.py
@@ -6,9 +6,9 @@ from selenium.webdriver.common.action_chains import ActionChains
 import time, os, pandas as pd
 
 # Integração à planilha
-studentsSpreadsheet = pd.read_excel(r".gitignore/primeiro.xlsx", header = 0)
+studentsSpreadsheet = pd.read_excel(r"sistemaBiblioteca-EEEP-AN\BibLivre5\2D.xlsx", header = 0)
 registrationUser    = studentsSpreadsheet['Matricula'].tolist()
-nameUser            = studentsSpreadsheet['Nomes'].tolist()
+nameUser            = studentsSpreadsheet['Nome'].tolist()
 cpfUser             = studentsSpreadsheet['CPF'].tolist()
 emailUser           = studentsSpreadsheet['Email'].tolist()
 genderUser          = studentsSpreadsheet['Genero'].tolist()
@@ -36,5 +36,24 @@ cursor = ActionChains(driver)
 cursor.move_to_element(menu_trigger).perform()
 popup_option = driver.find_element(By.XPATH, '/html/body/form/div[1]/div[6]/ul/li[2]/ul/li[1]').click()
 
+#Registro de novo usuário
+cont = 0
+for i in nameUser:
+    driver.find_element(By.XPATH, '/html/body/form/div[3]/div[1]/div[2]/div[3]/div[1]/div[4]/a').click()
+    driver.find_element(By.XPATH, '/html/body/form/div[3]/div[1]/div[2]/div[3]/div[4]/div[1]/div[1]/div/div[1]/div[2]/input').send_keys(f'{nameUser[cont]}')
+    driver.find_element(By.XPATH, '/html/body/form/div[3]/div[1]/div[2]/div[3]/div[4]/div[1]/div[1]/div/div[3]/div[2]/select').find_element(By.XPATH, '/html/body/form/div[3]/div[1]/div[2]/div[3]/div[4]/div[1]/div[1]/div/div[3]/div[2]/select/option[2]').click()
+    driver.find_element(By.XPATH, '/html/body/form/div[3]/div[1]/div[2]/div[3]/div[4]/div[2]/div/div/fieldset/div/div[1]/div[2]/input').send_keys(f'{emailUser[cont]}')
+    if genderUser[cont] == 'f':
+        driver.find_element(By.XPATH, '/html/body/form/div[3]/div[1]/div[2]/div[3]/div[4]/div[2]/div/div/fieldset/div/div[2]/div[2]/select').find_element(By.XPATH, '/html/body/form/div[3]/div[1]/div[2]/div[3]/div[4]/div[2]/div/div/fieldset/div/div[2]/div[2]/select/option[3]').click()
+    elif genderUser[cont] == 'm':
+        driver.find_element(By.XPATH, '/html/body/form/div[3]/div[1]/div[2]/div[3]/div[4]/div[2]/div/div/fieldset/div/div[2]/div[2]/select').find_element(By.XPATH, '/html/body/form/div[3]/div[1]/div[2]/div[3]/div[4]/div[2]/div/div/fieldset/div/div[2]/div[2]/select/option[2]').click()
+    driver.find_element(By.XPATH, '/html/body/form/div[3]/div[1]/div[2]/div[3]/div[4]/div[2]/div/div/fieldset/div/div[8]/div[2]/input').send_keys(f'{cpfUser[cont]}')
+    if genderUser[cont] == 'f':
+        driver.find_element(By.XPATH, '/html/body/form/div[3]/div[1]/div[2]/div[3]/div[4]/div[2]/div/div/fieldset/div/div[16]/div[2]/textarea').send_keys(f'Aluna de matrícula: {registrationUser[cont]}, do ano de {yearRegister}.')
+    elif genderUser[cont] == 'm':
+        driver.find_element(By.XPATH, '/html/body/form/div[3]/div[1]/div[2]/div[3]/div[4]/div[2]/div/div/fieldset/div/div[16]/div[2]/textarea').send_keys(f'Aluno de matrícula: {registrationUser[cont]}, do ano de {yearRegister}.')
+    driver.find_element(By.XPATH, '/html/body/form/div[3]/div[1]/div[2]/div[3]/div[4]/div[3]/div[2]/a[1]').click()
+    driver.find_element(By.XPATH, '/html/body/form/div[3]/div[1]/div[2]/div[3]/div[2]/a').click()
+    cont += 1
 
 driver.quit()
